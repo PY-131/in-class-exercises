e70830374648848c8bd3f13dbfa5336daabd76a9
dd-code-immersives <domilamad89@gmail.com>
2022-06-07 17:04:41 +0200

    implemented sorting ASC/DESC for letter count

--- word_info_cli.py
+++ word_info_cli.py
@@ -17,6 +17,7 @@ parser.add_argument("WORD")
 parser.add_argument("-lc","--letter_count", action="store_true", help="Counts letters")
 parser.add_argument("-vc","--vowel_count", action="store_true", help="Counts vowels")
 parser.add_argument("-cc","--consonants_count", action="store_true", help="counts consonants")
+parser.add_argument("-s", "--sorted", action="store", help="sort output", choices= ["ASC", "DESC"])
 args = parser.parse_args()
 
 
@@ -24,7 +25,14 @@ wd = WordInfo(args.WORD)
 
 if args.letter_count:
 	res = wd.populate_count_dict()	
-	pp(res)
+
+	if args.sorted:
+		sorting_ = True if args.sorted == "DESC" else False
+		res = sorted(res.items(), key=lambda x: x[1], reverse=sorting_)
+
+	for k,v in res:
+		print(f"letter: {k}, frequency: {v}")
+
 if args.vowel_count:
 	print(f"Vowl Count: {wd.count_vowels()}")
 
